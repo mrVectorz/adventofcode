c37f9cca94cab577ff42a1845cbf4d2ed82d015a
Marc Methot <mb.methot@gmail.com>
2020-12-08 18:56:59 -0500

    refactored to make use of slices instead of a fixed array
    This way is more dynamic and would work with other's input

--- 2020/day_8.go
+++ 2020/day_8.go
@@ -9,22 +9,26 @@ import (
 	"strconv"
 )
 
-// line count of the file
-//var lineCount = 612
+type instruction struct{
+	op string
+	arg string
+}
 
-func swap(operations [612][2]string, i int, op string)(instructions [612][2]string) {
-	newOperations := operations
+
+func swap(operations []instruction, i int, op string)(instructions []instruction) {
+	newOperations := make([]instruction, len(operations)) //when copying the slice needs to already have the right length
+	copy(newOperations, operations)
 	newOp := "jmp"
 	if op == "jmp" {
 		newOp = "nop"
 	}
-	newOperations[i][0] = newOp
+	newOperations[i].op = newOp
 	return newOperations
 }
 
-func partB(operations [612][2]string)(accumulator int) {
+func partB(operations []instruction)(accumulator int) {
 	for i := range operations {
-		op := operations[i][0]
+		op := operations[i].op
 		if op == "acc" {
 			continue
 		} else {
@@ -38,19 +42,30 @@ func partB(operations [612][2]string)(accumulator int) {
 	return 0
 }
 
-func partA(operations [612][2]string)(accumulator int, state string) {
-	var opCounter [612]int
+func partA(operations []instruction)(accumulator int, state string) {
+	opCounter := make([]int, len(operations))
+	// so we can't use `var opCounter [len(operators)]int`
+	// and the bellow is slow(ish)
+	/*
+	var opCounter []int
+	//build op array
+	for range operations {
+		opCounter = append(opCounter, 0)
+	}
+	*/
 	i := 0
 	for {
 		if i > len(operations)-1 {
 			return accumulator, "done"
 		}
+
 		opCounter[i] = opCounter[i]+1
 		if opCounter[i] == 2 {
 			return accumulator, "loops"
 		}
-		op := operations[i][0]
-		arg, _ := strconv.Atoi(operations[i][1])
+
+		op := operations[i].op
+		arg, _ := strconv.Atoi(operations[i].arg)
 		if op == "nop" {
 			i++
 		} else if op == "jmp" {
@@ -71,15 +86,16 @@ func main() {
 	}
 	defer file.Close()
 
-	var instructions [612][2]string
+	var instructions []instruction
 	i := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		operation := strings.Split(line, " ")
-		instructions[i][0] = operation[0]
-		instructions[i][1] = operation[1]
+		instructions = append(instructions, instruction{operation[0], operation[1]})
+		//instructions[i][0] = operation[0]
+		//instructions[i][1] = operation[1]
 		i++
 	}
 
